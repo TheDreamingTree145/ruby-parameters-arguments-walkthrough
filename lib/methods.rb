6125481e5667da72dc4c06d841533ac6b83b96c4
Dustin Anderson <dandersonsoftware@outlook.com>
2018-02-23 00:18:57 +0000

    basic setup for walkthrough

---
+++ lib/methods.rb
@@ -0,0 +1,12 @@
+# Define #candy_bars below
+# This method will take in two parameters an array filled with chocolate bar names and an index
+# It should output the element at the index passed into the method
+# The format of the out should be: '1. Payday' where index = 
+
+
+
+# Define #cartoons method below
+# Method will take in two parameters an index and an array of cartoon characters
+# Assume the array is sorted in order of favorite of characters
+# Output the rank and characters name of the first and last ranked characters
+
---
+++ spec/methods_spec.rb
@@ -0,0 +1,52 @@
+describe '#candy_bars' do
+  bars = ['Snickers', 'Payday', '100 Grand', 'Baby Ruth', 'Butterfinger']
+  index_one = 3
+  index_two = 1
+  
+  it 'accepts two arguments' do
+
+    allow(self).to receive(:puts)
+
+    expect { candy_bars(bars, index_one) }.to_not raise_error
+    expect { candy_bars(bars, index_two) }.to_not raise_error
+  end
+
+  it 'outputs out the candy bar at the index and the index passed into the argument' do
+    
+    expect {candy_bars(bars, index_one) }.to output("3. Baby Ruth\n").to_stdout
+
+  end
+  
+  it 'is not hardcoded' do 
+    
+    expect { candy_bars(bars, index_two) }.to output("1. Payday\n").to_stdout
+    
+  end
+  
+end
+
+describe '#cartoons' do 
+  characters = ['SpongeBob SquarePants', 'Eric Cartman', 'Snoopy', 'Bender', 'Philip J. Fry']
+  
+  it 'accepts two arguments' do
+    
+   allow(self).to receive(:puts)
+   
+   expect { cartoons(characters) }.to_not raise_error 
+    
+  end
+  
+  it 'outputs the first and last element of array in with their rank' do 
+    
+    expect { cartoons(characters) }.to output("1. SpongeBob SquarePants and 5. Philip J. Fry\n").to_stdout
+  
+  end
+  
+  it 'is not hardcoded' do
+    
+    characters = ["Doug Funnie", "Skeeter", "Peter Griffin", "Stewie Griffin"]
+    
+    expect { cartoons(characters) }.to output("1. Doug Funnie and 4. Stewie Griffin\n").to_stdout
+  end
+end
+
